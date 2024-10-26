520ac2a4b25f2dc0e33d186b7027c121aaac6b8a
Bartek-Figat <figat29@gmail.com>
2024-10-26 19:08:08 -0400

    GH work flow

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store as appStore } from "./store/store.ts";
 import { App } from "./App.tsx";
 import "./css/index.css";
 
-
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
- 
-      <Provider store={appStore}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-      </Provider>
-  </React.StrictMode>
+    <Provider store={appStore}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>,
 );
